d5f039b19c1aaf0f1f5886d6541cea8834a6d43e
Alexandru Ticu <ticu.alexandrum@gmail.com>
2021-11-11 19:49:13 +0200

    commit1

---
+++ src/components/Description.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { Typography } from "antd";
+const { Title } = Typography;
+  
+export const TitleDesc=()=>
+{
+  return (
+      <div>
+    <Title className="TitleName">webAppTest</Title>
+    <p>
+    </p>
+    </div>
+  );
+};
